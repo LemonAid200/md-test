8dfc953f74d2dfd70e371e4f9916896a85c43681
Ignatev Vsevolod <ignatevvsevolod1998@gmail.com>
2024-03-01 22:52:45 +0500

    created empty project

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <custom-table />
+  </div>
+</template>
+
+<script>
+import CustomTable from './components/CustomTable.vue'
+export default {
+  name: 'App',
+  components: { CustomTable },
+  data() {
+    return {
+      employess: [
+        {
+          imgURL: '',
+          name: 'Sarah Connor',
+          team: 'Crosty',
+          role: 'Team Lead',
+          gmail: 'sarahconnor@gmail.com',
+          birthday: '25.04.1994',
+          telegram: '@sarahconnor',
+          lastLogin: '03.11.2021'
+        },
+        {
+          imgURL: '',
+          name: 'John Doe',
+          team: 'Alpha',
+          role: 'Developer',
+          gmail: 'johndoe@gmail.com',
+          birthday: '12.09.1988',
+          telegram: '@johndoe',
+          lastLogin: '15.10.2021'
+        },
+        {
+          imgURL: '',
+          name: 'Alice Smith',
+          team: 'Beta',
+          role: 'Designer',
+          gmail: 'alicesmith@gmail.com',
+          birthday: '18.07.1990',
+          telegram: '@alicesmith',
+          lastLogin: '29.08.2021'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CustomTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="table-wrapper">
+    <div class="TeamInfo">Team info</div>
+    <button class="addEmployee">Add new employee</button>
+    <table>
+      <thead></thead>
+      <tbody></tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CustomTable',
+  props: {},
+  components: {}
+}
+</script>
+<style>
+.table-wrapper {
+  background: rgb(245, 247, 251);
+  padding: 28px;
+}
+.TeamInfo {
+}
+.addEmployee {
+}
+</style>
